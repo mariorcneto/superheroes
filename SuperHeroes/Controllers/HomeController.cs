96f6ef9cbeb95b8f50c08d9f11e27eca8e2cb5a1
Mario <mario@consinco.com.br>
2019-10-16 09:14:34 -0300

    first commit

---
+++ SuperHeroes/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using SuperHeroes.Models;
+
+namespace SuperHeroes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        public ActionResult Login(string name, string password)
+        {
+            //if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(password))
+            //    return RedirectToAction("Index");
+
+            ViewBag.login = false;
+            return View("Index", new User());
+
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
